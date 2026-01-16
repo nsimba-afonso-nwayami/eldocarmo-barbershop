a6c4970d77a53aa19bc3d754176b305e163ecb10
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-16 16:29:55 +0100

    Página sobre criada

---
+++ src/pages/site/Sobre.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import SobreImg from "../../assets/img/sobre.jpg";
+
+export default function Sobre() {
+  return (
+    <>
+      <title>Sobre | Eldocarmo Barbershop</title>
+      {/* Header */}
+      <Header />
+      
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
