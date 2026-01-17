8bdb50bb9356ac8d305c51e9f42a7a5ab0073ba5
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-17 20:23:21 +0100

    Botão de login adicionado

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -40,13 +40,10 @@ export default function Header() {
             overflow-hidden
             transition-all duration-300 ease-in-out
             md:overflow-visible md:max-h-full md:opacity-100 md:translate-y-0
-            ${
-              menuOpen
-                ? "max-h-96 opacity-100 translate-y-0 py-6"
-                : "max-h-0 opacity-0 -translate-y-2 py-0"
-            }
+            ${menuOpen ? "max-h-250 opacity-100 translate-y-0 py-6" : "max-h-0 opacity-0 -translate-y-2 py-0"}
           `}
         >
+          {/* Links principais */}
           <Link to="/" className="hover:text-amber-400 transition-colors">
             Início
           </Link>
@@ -78,16 +75,26 @@ export default function Header() {
             Contato
           </Link>
 
-          {/* CTA WhatsApp */}
-          <a
-            href="https://wa.me/244000000000"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-amber-400 text-stone-50 px-4 py-2 rounded-lg hover:bg-amber-500 transition flex items-center gap-2"
-          >
-            <i className="fab fa-whatsapp"></i>
-            WhatsApp
-          </a>
+          {/* Botões CTA */}
+          <div className="flex flex-col md:flex-row w-full md:w-auto gap-4 mt-4 md:mt-0">
+            <Link
+              to="/login"
+              className="border border-amber-400 text-amber-400 px-4 py-2 rounded-lg hover:bg-amber-400 hover:text-stone-900 transition flex items-center justify-center gap-2 w-full md:w-auto"
+            >
+              <i className="fas fa-user"></i>
+              Login
+            </Link>
+
+            <a
+              href="https://wa.me/244000000000"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-amber-400 text-stone-50 px-4 py-2 rounded-lg hover:bg-amber-500 transition flex items-center justify-center gap-2 w-full md:w-auto"
+            >
+              <i className="fab fa-whatsapp"></i>
+              WhatsApp
+            </a>
+          </div>
         </nav>
       </div>
     </header>
