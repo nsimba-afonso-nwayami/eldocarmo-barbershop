26934b734852760756d110521e971b121d057aa5
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-18 00:15:13 +0100

    Rotas de autenticação adicionadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,6 +10,8 @@ import NotFound from "../pages/site/NotFound";
 
 //Auth
 import Login from "../pages/auth/Login";
+import Cadastrar from "../pages/auth/Cadastrar";
+import EsqueceuSenha from "../pages/auth/EsqueceuSenha";
 
 export default function AppRoutes() {
   return (
@@ -25,6 +27,10 @@ export default function AppRoutes() {
 
       {/*Rotas de auth */}
       <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/cadastrar" element={<Cadastrar />} />
+      <Route path="/auth/esqueceu-senha" element={<EsqueceuSenha />} />
+
+      {/*Rotas do dashboard admin */}
     </Routes>
   );
 }
