eb98db9b9ed220fd37fc9c8fde8beda920fc7085
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-20 16:51:30 +0100

    Página not found do profissional criada

---
+++ src/pages/profissional/NotFoundProfissional.jsx
@@ -0,0 +1,12 @@
+import ProfissionalLayout from "./components/ProfissionalLayout";
+import { Link } from "react-router-dom";
+
+export default function NotFoundProfissional() {
+  return (
+    <>
+      <title>Dashboard Profissional | Eldocarmo Barbershop</title>
+
+      <ProfissionalLayout title="Dashboard Profissional"></ProfissionalLayout>
+    </>
+  );
+}
