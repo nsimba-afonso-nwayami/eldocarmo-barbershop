27b3fef36b5ed3c8433e885b470868876e14a802
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-20 14:43:47 +0100

    P[agina de perfil criada

---
+++ src/pages/admin/PerfilAdmin.jsx
@@ -0,0 +1,12 @@
+import AdminLayout from "./components/AdminLayout";
+import { Link } from "react-router-dom";
+
+export default function PerfildAdmin() {
+  return (
+    <>
+      <title>Perfil | Eldocarmo Barbershop</title>
+
+      <AdminLayout title="Perfil"></AdminLayout>
+    </>
+  );
+}
