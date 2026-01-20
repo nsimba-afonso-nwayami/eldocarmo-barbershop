c3b2397ec617b25b3aaf3da2e1e1a95f0c9a2a57
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-20 16:55:44 +0100

    Página dashboard do profissional criada

---
+++ src/pages/profissional/DashboardProfissional.jsx
@@ -0,0 +1,14 @@
+import ProfissionalLayout from "./components/ProfissionalLayout";
+import { Link } from "react-router-dom";
+
+export default function DashboardProfissional() {
+  return (
+    <>
+      <title>Dashboard Profissional | Eldocarmo Barbershop</title>
+
+      <ProfissionalLayout title="Dashboard Profissional">
+        
+      </ProfissionalLayout>
+    </>
+  );
+}
