c7339c28d9a2d899d964c8bcf89f9e69025af2b8
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-21 09:13:23 +0100

    Página not found do cliente criada

---
+++ src/pages/cliente/NotFoundCliente.jsx
@@ -0,0 +1,15 @@
+import ClienteLayout from "./components/ClienteLayout";
+import { Link } from "react-router-dom";
+
+export default function NotFoundCliente() {
+
+  return (
+    <>
+      <title>Página não encontrada | Eldocarmo Barbershop</title>
+
+      <ClienteLayout title="Página não encontrada">
+        
+      </ClienteLayout>
+    </>
+  );
+}
