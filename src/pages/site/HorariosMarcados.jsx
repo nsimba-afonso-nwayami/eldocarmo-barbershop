5b67817a93386835d2aa81ad2b71e17043909db9
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-22 15:33:50 +0100

    Página de horários marcados criada

---
+++ src/pages/site/HorariosMarcados.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function HorariosMarcados() {
+
+  return (
+    <>
+      <title>Horários Marcados | Eldocarmo Barbershop</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
