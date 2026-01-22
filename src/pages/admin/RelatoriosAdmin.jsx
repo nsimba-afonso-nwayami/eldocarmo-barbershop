9ef209aeebc41b3420e09df5c8a94ae79b832d1f
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-22 14:03:44 +0100

    Página relatórios  do admin criada

---
+++ src/pages/admin/RelatoriosAdmin.jsx
@@ -0,0 +1,14 @@
+import AdminLayout from "./components/AdminLayout";
+import { Link } from "react-router-dom";
+
+export default function RelatoriosAdmin() {
+  return (
+    <>
+      <title>Relatórios | Eldocarmo Barbershop</title>
+
+      <AdminLayout title="Relatórios">
+        
+      </AdminLayout>
+    </>
+  );
+}
